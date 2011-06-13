03a8d0214b6fc1d934e53010e8c90817535f556f
alexis.kiosia@gmail.com <alexis.kiosia@gmail.com@214650db-1146-01cc-221b-933368280467>
2011-06-13 22:00:00 +0000

    Botando MD5 no encode!

--- COO_02/src/User.java
+++ COO_02/src/User.java
@@ -1,4 +1,5 @@
 import java.util.Vector;
+import java.security.MessageDigest;
 
 /**
  * @author Aléxis Kiosia (7152097)
@@ -69,7 +70,17 @@ public class User {
 	 * @return A senha codificada. 
 	 */
 	private String encode(String pass) {
-		return pass;
+		MessageDigest md = MessageDigest.getInstance("MD5");
+		md.update(pass.getBytes());
+		byte[] bytes = md.digest();
+		StringBuilder s = new StringBuilder();
+		   for (int i = 0; i < bytes.length; i++) {
+		       int parteAlta = ((bytes[i] >> 4) & 0xf) << 4;
+		       int parteBaixa = bytes[i] & 0xf;
+		       if (parteAlta == 0) s.append('0');
+		       s.append(Integer.toHexString(parteAlta | parteBaixa));
+		   }
+		   return s.toString();
 	}
 	
 	public String getName() {
